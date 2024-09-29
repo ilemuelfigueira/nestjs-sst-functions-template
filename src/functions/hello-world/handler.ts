9eddfd1c0bbbe689e3543513c4ee4fed53b0a40b
ilemuelfigueira <lfigueiradev@gmail.com>
2024-09-29 00:59:40 -0300

    ajustes handler e lib start-engine

--- src/functions/hello-world/handler.ts
+++ src/functions/hello-world/handler.ts
@@ -2,15 +2,27 @@ import type { INestApplication } from '@nestjs/common';
 import { HelloWorldModule } from './hello-world.module';
 import { startEngine } from '@infra/utils/start-engine';
 import { IHelloWorldUseCase } from '@domain/use-cases/hello-world/hello-world.use-case';
+import type { APIGatewayProxyHandlerV2 } from 'aws-lambda';
 
 let instance: INestApplication;
 
-export async function handler() {
-  instance = await startEngine(HelloWorldModule, instance);
-
-  const useCase = instance.get<IHelloWorldUseCase>(IHelloWorldUseCase);
-
-  await useCase.execute();
+export const welcome: APIGatewayProxyHandlerV2 = async () => {
+  try {
+    instance = await startEngine(HelloWorldModule, instance);
+  
+    const useCase = instance.get<IHelloWorldUseCase>(IHelloWorldUseCase);
+  
+    await useCase.execute();
+  
+    return {
+      statusCode: 200,
+      body: 'Hello world from nestjs'
+    }
+  } catch (error) {
+    console.error('Erro inesperado', error)
+    return {
+      statusCode: 500,
+      body: `Erro inesperado: ${error}`
+    }
+  }
 }
-
-handler();
--- src/infraestructure/utils/start-engine.ts
+++ src/infraestructure/utils/start-engine.ts
@@ -2,9 +2,13 @@ import type { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 export async function startEngine(module: any, instance: INestApplication) {
-  if (instance) return instance;
-
-  instance = await NestFactory.create(module);
-
-  return instance;
+  try {
+    if (instance) return instance;
+  
+    instance = await NestFactory.create(module);
+  
+    return instance;
+  } catch (error) {
+    throw error
+  }
 }
